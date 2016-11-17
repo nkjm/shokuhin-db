bc076fbbad1b9b5ffd65d72c7ad0538afca242ac
Kazuki Nakajima <nkjm@Kazukis-MacBook-Air.local>
2016-11-17 16:09:54 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# 概要
+
+食品のカロリー、栄養量を取得するためのライブラリです。
+食品名をキーワードに問い合わせをおこない、該当する食品がデータベースにあればそのカロリー・栄養量が返されます。
+食品のデーターベースは文部科学省が公開している[日本食品標準成分表2015年版](http://www.mext.go.jp/a_menu/syokuhinseibun/1365295.htm)のデータを利用しています。
+
+# インストール
+
+```
+$ npm install shokuhin-db
+```
+
+# 利用方法
+
+```
+const shokuhinDb = require('shokuhin-db');
+
+shokuhinDb.getNutrition('メロンパン')
+.then(
+    function(response){
+        console.log(response);
+    },
+    function(error){
+        console.log(error);
+    }
+);
+```
---
+++ module/shokuhin-db.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const request = require('request');
+const Promise = require('bluebird');
+const apiBase = 'https://apex.oracle.com/pls/apex/evangelist/shokuhindb';
+
+module.exports = class shokuhinDb {
+
+    static getNutrition(foodName){
+        return new Promise(function(resolve, reject){
+            const headers = {
+                'Content-Type': 'application/json'
+            };
+            const url = dbPrefix + '/food/' + encodeURIComponent(foodName);
+            request({
+                url: url,
+                method: 'GET',
+                headers: headers,
+                json: true
+            }, function (error, response, body) {
+                if (error){
+                    reject(error);
+                    return;
+                }
+                if (typeof body.items == 'undefined' || response.statusCode != 200){
+                    reject({message:'Failed to get nutrition.'});
+                    return;
+                }
+                resolve(body.items);
+                return;
+            });
+        });
+    }
+};
